b608aa4499c8e6ba0ef5ef3fdb25486d1b91afe8
Arthur Golubtsov <goldartt@gmail.com>
2019-07-18 17:32:49 +0100

    Update code to working state, remove unnecessary file

--- src/vive_frame_publisher.cpp
+++ src/vive_frame_publisher.cpp
@@ -1,15 +1,17 @@
 #include <libsurvive/survive_api.h>
+#include <unordered_map>
 #include <ros/ros.h>
 #include <tf2_ros/buffer.h>
 #include <tf2_ros/transform_broadcaster.h>
-#include <geometry_msgs/PoseStamped.h>
-#include <unordered_map>
+#include <tf2_ros/transform_listener.h>
+#include <tf2_ros/static_transform_broadcaster.h>
 #include <tf2/LinearMath/Quaternion.h>
 #include <tf2_geometry_msgs/tf2_geometry_msgs.h>
-#include <tf2_ros/static_transform_broadcaster.h>
 #include <geometry_msgs/TransformStamped.h>
+#include <geometry_msgs/PoseStamped.h>
 
-std::string baseFrameId;
+
+std::string base_frame_id;
 double yaw, pitch, roll, yaw2, pitch2, roll2, yaw_static, pitch_static, roll_static;
 
 int main(int argc, char** argv)
@@ -18,13 +20,10 @@ int main(int argc, char** argv)
 	SurviveSimpleContext *ctx = survive_simple_init(argc, argv);
 
 	ros::NodeHandle nh("~");
-	nh.param<std::string>("base_frame_id", baseFrameId, "map");
+	nh.param<std::string>("base_frame_id", base_frame_id, "map");
 	nh.param<double>("yaw", yaw, 0.0);
 	nh.param<double>("pitch", pitch, 0.0);
 	nh.param<double>("roll", roll, 0.0);
-	nh.param<double>("yaw2", yaw2, 0.0);
-	nh.param<double>("pitch2", pitch2, 0.0);
-	nh.param<double>("roll2", roll2, 0.0);
 	nh.param<double>("yaw_static", yaw_static, 0.0);
 	nh.param<double>("pitch_static", pitch_static, 0.0);
 	nh.param<double>("roll_static", roll_static, 0.0);
@@ -35,14 +34,17 @@ int main(int argc, char** argv)
 	geometry_msgs::PoseStamped poseMsg;
 	static tf2_ros::StaticTransformBroadcaster static_broadcaster;
     geometry_msgs::TransformStamped static_transformStamped;
+	tf2_ros::Buffer tf_buffer;
+    tf2_ros::TransformListener tf_Listener(tf_buffer);
 
-	//std::string baseFrameId = "base_link"; // TODO: pass base frame ID as a parameter
+	//std::string base_frame_id = "base_link"; // TODO: pass base frame ID as a parameter
 
 	survive_simple_start_thread(ctx);
 
     tf2::Quaternion q_orig, q_rot, q_rot2, q_new, q_static;
 	ros::Publisher T20_t1 = nh.advertise<geometry_msgs::PoseStamped>("T20_pose_t1", 1);
 	ros::Publisher T20_t2 = nh.advertise<geometry_msgs::PoseStamped>("T20_pose_t2", 1);
+	geometry_msgs::TransformStamped transform, vive_transform;
 	ros::Rate pub_rate(30);
 
 	auto getPublisher = [&](const char *name) -> ros::Publisher& {
@@ -70,21 +72,19 @@ int main(int argc, char** argv)
 			const char *name = survive_simple_object_name(it);
 			std::string str_name = name;
 			std::string t20_name = "T20";
-			geometry_msgs::TransformStamped transform;
-
 			uint32_t timestamp = survive_simple_object_get_latest_pose(it, &pose);
 
 			q_orig = tf2::Quaternion(pose.Rot[1], pose.Rot[2], pose.Rot[3], pose.Rot[0]);
 			q_orig.normalize(); 
 			
 			// Stuff the new rotation back into the pose. This requires conversion into a msg type
-			poseMsg.header.frame_id = baseFrameId;
+			poseMsg.header.frame_id = base_frame_id;
 			poseMsg.pose.position.x = pose.Pos[0];
 			poseMsg.pose.position.y = pose.Pos[1];
 			poseMsg.pose.position.z = pose.Pos[2];
 			tf2::convert(q_orig, poseMsg.pose.orientation);
 
-			transform.header.frame_id = baseFrameId;
+			transform.header.frame_id = base_frame_id;
 			transform.child_frame_id = name;
 			transform.transform.translation.x = pose.Pos[0];
 			transform.transform.translation.y = pose.Pos[1];
@@ -106,26 +106,25 @@ int main(int argc, char** argv)
   				q_static.setRPY(roll_static, pitch_static, yaw_static);
 				tf2::convert(q_static, static_transformStamped.transform.rotation);
 				static_broadcaster.sendTransform(static_transformStamped);
+				
+				try {
+					vive_transform = tf_buffer.lookupTransform(base_frame_id, "T20_t1", static_transformStamped.header.stamp, ros::Duration(0.02));
+					poseMsg.header.frame_id = base_frame_id;
+					poseMsg.pose.position.x = vive_transform.transform.translation.x;
+					poseMsg.pose.position.y = vive_transform.transform.translation.y;
+					poseMsg.pose.position.z = vive_transform.transform.translation.z;
+					poseMsg.pose.orientation = vive_transform.transform.rotation;
+					T20_t1.publish(poseMsg);
+				} catch (tf2::TransformException &ex) {
+    				ROS_WARN("Could NOT transform: %s", ex.what());
+  				}
 
-				poseMsg.header.frame_id = "T20_t1";
-				poseMsg.pose.position.x = 0;
-				poseMsg.pose.position.y = 0;
-				poseMsg.pose.position.z = 0;
-				q_static.setRPY(0, 0, 0);
-				tf2::convert(q_static, poseMsg.pose.orientation);
-				T20_t1.publish(poseMsg);
+				//vive_transform = tf_buffer.lookupTransform(base_frame_id, "T20_t1", static_transformStamped.header.stamp, ros::Duration(0.02));				
 
+				/*
 				q_rot.setRPY(roll, pitch, yaw);
-				q_rot2.setRPY(roll2,pitch2, yaw2);
 				q_new = q_rot*q_static;
-				q_new = q_rot2*q_new;
 				q_new.normalize();
-				poseMsg.header.frame_id = "T20_t1";
-				poseMsg.pose.position.x = 0;
-				poseMsg.pose.position.y = 0;
-				poseMsg.pose.position.z = 0;
-				tf2::convert(q_new, poseMsg.pose.orientation);
-				T20_t2.publish(poseMsg);
 
 				transform.header.frame_id = "T20_t1";
 				transform.child_frame_id = "T20_t2";
@@ -134,9 +133,17 @@ int main(int argc, char** argv)
 				transform.transform.translation.z = 0;
 				tf2::convert(q_new, transform.transform.rotation);
 				br.sendTransform(transform);
+
+				poseMsg.header.frame_id = "T20_t2";
+				poseMsg.pose.position.x = 0;
+				poseMsg.pose.position.y = 0;
+				poseMsg.pose.position.z = 0;
+				q_static.setRPY(0, 0, 0);
+				tf2::convert(q_static, poseMsg.pose.orientation);
+				T20_t2.publish(poseMsg);
+				*/
 			}
 		}
-
 		ros::spinOnce();
 		pub_rate.sleep();
 
